ce89853bfa6382e850e21c8a31fdb3bafde6d812
Holutunde <Holutunde99@gmail.com>
2023-05-03 22:18:52 +0100

    restructure

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const morgan = require("morgan");
+// Import required modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 require("dotenv").config();
+
+// Import database module and survey route
 const connectDB = require("./database/db");
 const surveyRoute = require("./routes/SurveyRoute");
 
+// Create express app
 const app = express();
 
+// Set port
 const port = process.env.PORT || 4000;
 
+// Middleware
 app.use(bodyParser.json());
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(morgan("dev"));
 
+// Set survey route
 app.use("/survey", surveyRoute);
 
+// Start server
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
   } catch (error) {
     console.log(error);
   }
